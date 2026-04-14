fe3a0b8e73b5a9597d0a8c7daef3678faab3d764
Larsvestby <Larrsvestby@gmail.com>
2026-04-14 17:41:05 +0200

    .

--- CalorieTrackerTests/obj/Debug/net9.0/CalorieTrackerTests.AssemblyInfo.cs
+++ CalorieTrackerTests/obj/Debug/net9.0/CalorieTrackerTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalorieTrackerTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da03f59279536a2b1fead651a001d10aed8223d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8726a679446994dfb8fc870180556228a0e4f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalorieTrackerTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalorieTrackerTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
