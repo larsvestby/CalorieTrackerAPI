ac3c724a6f160783bc077c1399bae0c5da2d61b7
Larsvestby <Larrsvestby@gmail.com>
2026-04-14 17:08:05 +0200

    Fix solution and test project structure for CI

--- CalorieTrackerTests/obj/Debug/net9.0/CalorieTrackerTests.AssemblyInfo.cs
+++ CalorieTrackerTests/obj/Debug/net9.0/CalorieTrackerTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalorieTrackerTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a862021edd607241ae964e7114d9d00f87d8f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalorieTrackerTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalorieTrackerTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
